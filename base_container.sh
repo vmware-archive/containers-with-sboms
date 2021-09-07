03ce5e0c33ac3324179c222ed915e50c9c064cdd
Nisha K <nishak@vmware.com>
2021-09-07 06:33:02 -0700

    Verify artifacts with cosign
    This assumes we have set up cosign key-pairs in a directory called
    "cosign" with the password exported.
    
    I haven't automated that part, because it's a password.
    
    Signed-off-by: Nisha K <nishak@vmware.com>

--- base_container.sh
+++ base_container.sh
@@ -13,17 +13,27 @@ ctr=$(buildah from scratch)
 echo creating container image...
 mnt=$(buildah unshare buildah mount $ctr)
 buildah add $ctr debian.tar
+
 # We already know this is a debian 10 minbase rootfs so
 # we will name the image accordingly
 img=$(buildah commit $ctr localhost:5000/debian:10)
+
 # We then provide this directory to tern
 echo generating sbom...
 tern report --live $mnt -f spdxjson -o debian-sbom
 echo image: localhost:5000/debian:10
 echo sbom: debian-sbom
+
 # Now push the image and the sbom
 buildah push --tls-verify=false localhost:5000/debian:10
 oras push localhost:5000/debian-sbom:10 debian-sbom:application/json
+
+# Let's sign our artifacts
+# This assumes a cosign keypair has been generated and exists
+# in a directory called "cosign"
+cosign sign -key ~/cosign/cosign.key localhost:5000/debian:10
+cosign sign -key ~/cosign/cosign.key localhost:5000/debian-sbom:10
+
 # clean up all the running containers
 buildah rm --all
 echo ready.
--- derived_container.sh
+++ derived_container.sh
@@ -9,17 +9,26 @@
 # and SBOM file called "sbom1" and a config file called sbom_config2.json
 
 # We create a container image using buildah from the previously built debian:10 image
+
 echo starting...
-ctr=$(buildah from localhost:5000/debian:10)
+# let's verify if that image is signed:
+cosign verify -key ~/cosign/cosign.pub localhost:5000/debian:10
+echo verified image
+# Assuming this image has been signed, let's start building a
+# container
 echo building container...
+ctr=$(buildah from localhost:5000/debian:10)
 mnt=$(buildah unshare buildah mount $ctr)
 
 # Let's install some stuff
 buildah unshare buildah run $ctr /bin/bash -c "apt-get update && apt-get install -y python3"
+
 # Let's commit this "python" container
 img=$(buildah commit $ctr localhost:5000/python:3)
 
 # Let's now download our image's corresponding sbom
+cosign verify -key ~/cosign/cosign.pub localhost:5000/debian-sbom:10
+echo verified sbom
 oras pull localhost:5000/debian-sbom:10 -a
 
 # We now provide this mount point to tern with the previous sbom
@@ -30,6 +39,10 @@ buildah push --tls-verify=false localhost:5000/python:3
 # We push both the sboms so we have one tag referencing all the sboms
 # related to this image
 oras push localhost:5000/python-sbom:3 debian-sbom:application/json python-sbom:application/json
+
+# Let's sign our new artifacts
+cosign sign -key ~/cosign/cosign.key localhost:5000/python:3
+cosign sign -key ~/cosign/cosign.key localhost:5000/python-sbom:3
 
 # Clean up all the containers
 buildah rm --all
